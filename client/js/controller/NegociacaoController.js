e48288a3e96818e96b89c017ee85651dfa69e3f3
tokyozinn <andre.yabiku@hotmail.com>
2020-02-20 11:50:30 -0300

    creating a defensive way to create and protect a list of negotiations

--- client/js/controller/NegociacaoController.js
+++ client/js/controller/NegociacaoController.js
@@ -5,17 +5,33 @@ class NegociacaoController {
         this.inputData = $('#data');
         this.inputQuantidade = $('#quantidade');
         this.inputValor = $('#valor');
+        this._listaNegociacoes = new ListaNegociacoes();
     }
 
     adiciona(event){
         event.preventDefault();
+        
+        this._listaNegociacoes.adiciona(this._criaNegociacoes());
+        this._limpaForm();    
+        
+        console.log(this._listaNegociacoes.negociacoes)
+    }
 
-        let negociacao = new Negociacao(
+    _criaNegociacoes(){
+
+        return new Negociacao(
             DateHelper.textoParaData(this.inputData.value),
             this.inputQuantidade.value,
-            this.inputValor.value
-        );
+            this.inputValor.value);
+    }
 
+    _limpaForm(){
+
+        this.inputData.value = '';
+        this.inputQuantidade.value = 1;
+        this.inputValor.value = 0.0;
+
+        this.inputData.focus();
     }
 
 }
